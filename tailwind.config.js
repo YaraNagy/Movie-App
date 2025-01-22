48236563dfe4c0e65611e437b5067b3ccc35d54c
Mohamed Ramadan <midoramadan100@yahoo.com>
2025-01-22 19:14:28 +0200

    Add the styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,10 @@ module.exports = {
     extend: {
       backgroundImage: {
         contactBgImg: "url('/images/cinema.jpg')"
-      }
+      } , 
+      colors: {
+        darkBlack: '#0A0A0A', // Custom color
+      },
     },
     keyframes: {
       slideInFromLeft: {
